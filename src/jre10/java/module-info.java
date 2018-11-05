34f77886bf3c6defdea199b144e4fb3e3d92d193
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:17 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,9 +1,12 @@
 import com.jwebmp.core.base.angular.services.IAngularController;
 import com.jwebmp.core.base.angular.services.IAngularModule;
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.angularuiselect.AngularUISelectDefaultConfigController;
 import com.jwebmp.plugins.angularuiselect.AngularUISelectModule;
 import com.jwebmp.plugins.angularuiselect.AngularUISelectPageConfigurator;
+import com.jwebmp.plugins.angularuiselect.implementations.AngularUISelectExclusionsModule;
 
 module com.jwebmp.plugins.angularuiselect {
 	exports com.jwebmp.plugins.angularuiselect;
@@ -11,11 +14,15 @@ module com.jwebmp.plugins.angularuiselect {
 
 	requires com.jwebmp.core;
 	requires java.validation;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with AngularUISelectPageConfigurator;
 	provides IAngularModule with AngularUISelectModule;
 	provides IAngularController with AngularUISelectDefaultConfigController;
 
-	opens com.jwebmp.plugins.angularuiselect to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.angularuiselect.interfaces to com.fasterxml.jackson.databind,com.jwebmp.core;
+	provides IGuiceScanJarExclusions with AngularUISelectExclusionsModule;
+	provides IGuiceScanModuleExclusions with AngularUISelectExclusionsModule;
+
+	opens com.jwebmp.plugins.angularuiselect to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.angularuiselect.interfaces to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
---
+++ src/main/java/com/jwebmp/plugins/angularuiselect/implementations/AngularUISelectExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.angularuiselect.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AngularUISelectExclusionsModule
+		implements IGuiceScanModuleExclusions<AngularUISelectExclusionsModule>,
+				           IGuiceScanJarExclusions<AngularUISelectExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-angular-ui-select-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.angularuiselect");
+		return strings;
+	}
+}
